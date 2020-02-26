961dbf4f47681d334899379287e1e4794bbaaffc
Daiane Souza <dai2704@gmail.com>
2020-02-25 20:55:07 -0500

    Updates

---
+++ precache-manifest.8b0ffca36883945971b8f77910899beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d8053f71bfe81c1605afba1531ad54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44da5211305da6cc4c18",
+    "url": "/static/css/main.776a3f60.chunk.css"
+  },
+  {
+    "revision": "b09bf446a4aa13da699c",
+    "url": "/static/js/2.6c88390c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6c88390c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44da5211305da6cc4c18",
+    "url": "/static/js/main.ad6b34a1.chunk.js"
+  },
+  {
+    "revision": "4ad63394c54636ce883a",
+    "url": "/static/js/runtime-main.39f08572.js"
+  }
+]);
